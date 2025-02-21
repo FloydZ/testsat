ec1d2e5c0a8d3a4ec759fd0d66d72bee7f55a7bf
Floyd Zweydinger <zweydfg8+github@rub.de>
2025-02-21 17:39:38 +0100

    now

---
+++ README.md
@@ -0,0 +1,11 @@
+TODO:
+=====
+- add CI
+- limit run of each solver to 1min or so 
+- somehow keep track of which solver solved what in what time
+
+
+NOTE:
+=====
+
+`./dictionary` is a optimized dictionary for zstd
--- run.sh
+++ run.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env bash
 builder=("build_kissat" "build_parafrost_cpu")
-runner=("run_kissat" "run_parafrost_cpu")
+runner=("run_kissat") # "run_parafrost_cpu")
 
-#cd solver
-#for b in "${builder[@]}"
-#do 
-#    ./${b}
-#done
-#cd ..
+out_folder="results/CI"
+results_file="${out_folder}/results"
+touch ${results_file}
 
-
+# to make sure that the CI doesnt run for ever
+counter=0
 for file in instances/*.xz
 do
+    # -k: keep, -d: decompress
     xz -kd ${file}
+    # remove the file ending
     file2=${file%.xz}
+    # remove the path to only get the filename
+    filename=${file2##*/}
+    # go through each runner
     for r in "${runner[@]}"
     do 
-        ./solvers/${r}.sh ${file2} > ${file2}.${r}
+        out_file="${out_folder}/${filename}.${r}"
+        if [ ! -f ${out_file} ]; then
+            echo "Running: ${out_file}"
+            # run the solver 
+            s=`date +%s.%N`
+            ./solvers/${r}.sh ${file2} > ${out_file}
+            e=`date +%s.%N`
+            runtime=$( echo "${e} - ${s}" | bc -l )
+
+            # check if the instance was solved or not
+            unsat=$(grep -F "s UNSATISFIABLE" ${out_file})
+            sat=$(grep -F "s SATISFIABLE" ${out_file})
+            unsat="${unsat/s /}"
+            sat="${sat/s /}"
+            
+            # write  the result back
+            echo ${filename}.${r} ${unsat} ${sat} ${runtime} >> ${results_file}
+        fi
     done
-    #rm *.cnf
-done
+
+    rm instances/*.cnf
+    counter=$((counter+1))
+    if [[ "$counter" -gt 20 ]]; then
+       echo "Counter: $counter times reached; Exiting loop!"
+       exit 0
+    fi
+done 
